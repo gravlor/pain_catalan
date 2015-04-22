b773faf00ea0318f197901d247a05bd4a9c53711
Laurent <laurent.andral@gmail.com>
2015-04-22 11:00:13 +0200

    dao begining ...

---
+++ Pain Catalan/src/paincatalan/dao/GenericDAO.java
@@ -0,0 +1,58 @@
+package paincatalan.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+
+
+public abstract class GenericDAO<T> {
+	
+	private Class<T> entityClass;
+    private Class<T> getEntityClass(){
+    	return entityClass;
+    }
+
+    public GenericDAO(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+    
+    @PersistenceContext
+    private EntityManager em;
+   
+    protected EntityManager getEntityManager() {
+    	if(em==null){
+    		EntityManagerFactory emf=Persistence.createEntityManagerFactory("Projet_Java");
+    		em=emf.createEntityManager();
+    	}
+		return em;
+	}
+
+	public void create(T entity) {
+       getEntityManager().persist(entity);
+		
+    }
+
+    public void edit(T entity) {
+        getEntityManager().merge(entity);
+    }
+
+    public void remove(T entity) {
+        getEntityManager().remove(getEntityManager().merge(entity));
+    }
+
+    public T find(Object id) {
+        return getEntityManager().find(getEntityClass(), id);
+    }
+
+    public List<T> findAll() {
+    	
+    	CriteriaQuery<T> cq = (CriteriaQuery<T>) em.getCriteriaBuilder().createQuery();
+       cq.select(cq.from(entityClass));
+       return em.createQuery(cq).getResultList();
+    }
+
+}
--- Pain Catalan/src/paincatalan/dao/UserDAO.java
+++ Pain Catalan/src/paincatalan/dao/UserDAO.java
@@ -1,5 +1,31 @@
 package paincatalan.dao;
 
-public class UserDAO {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import paincatalan.model.User;
+
+public class UserDAO extends GenericDAO<User> {
+
+	public UserDAO() {
+		super(User.class);
+	}
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@SuppressWarnings("unchecked")
+	public List<String> getAllCategories() {
+		return (List<String>) getEntityManager().createNamedQuery("Article.findAllCategories")
+				.getResultList();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<User> getAllProductsPerCategory(String categorie){
+		return (List<User>) getEntityManager().createNamedQuery("Article.findAllArticleByCategories").
+				setParameter("category",categorie).getResultList();
+	}
 
 }
